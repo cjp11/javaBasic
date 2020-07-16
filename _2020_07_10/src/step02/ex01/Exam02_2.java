b5f5c59e86f3c7d32abeef31d4fe3a558a528373
bitcamp <bitcamp@DESKTOP-2NJSMDI>
2020-07-16 20:40:24 +0900

    re

--- _2020_07_10/src/step02/ex01/Exam02_2.java
+++ _2020_07_10/src/step02/ex01/Exam02_2.java
@@ -3,7 +3,8 @@ package step02.ex01;
 public class Exam02_2 {
 
 	public static void main(String[] args) {
-		String s1 = new String("hello, world!");	// 힙에 잡힘
+		String s1 = new String("hello, world!");	// method영역에 문자열이 리터럴로 잡힌 후 
+													// 이 문자열을 복사해 힙에 잡음
 		System.out.println(s1);
 		
 		String s2 = "hello, world!";	// 메소드 영역의 상수 영역에 잡힘
